dc6f9f3792ca640fa8cdf74ab5e6cef8f59339b9
Abousaid Salah-Eddine <abousaid7salah@gmail.com>
2019-02-06 08:32:47 +0100

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+ // https://www.food2fork.com/api/search
+// Api key : 67b11f2842d5dee4be5a2ca297697006 
+ //const proxy = 'https://crossorigin.me/';
+
+ export const proxy = 'https://cros-anywhere.herokuapp.com/';
+ export const key = '67b11f2842d5dee4be5a2ca297697006';
